157e79044520b6542b7b802b908d3207ba3cefed
Ohminami.masato <dainan@ntts-sv.co.jp>
2013-02-18 16:10:14 +0900

    TestingチュートリアルのSpinnerTestクラスの最初のコミット
    SpiinnerActivityクラスへの参照エラーが中々とれずimport文を差し替えてOK
    理由は不明！！

---
+++ SpinnerActivityTest/src/com/android/example/spinner/test/SpinnerActivityTest.java
@@ -0,0 +1,13 @@
+package com.android.example.spinner.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.android.example.spinner.SpinnerActivity;
+
+public class SpinnerActivityTest extends
+		android.test.ActivityInstrumentationTestCase2<SpinnerActivity> {
+
+	public SpinnerActivityTest() {
+		super("com.android.example.spinner",SpinnerActivity.class);
+		}
+	// end of SpinnerActivityTest constructor definition
+}
